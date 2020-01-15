32515e0e696e38074d6fc78c36d4af03c1d27813
Paweł Migo <migo.pawel@gmail.com>
2020-01-15 16:24:32 +0100

    zakonczone

---
+++ main.js
@@ -0,0 +1,11 @@
+var num1 = 2;
+var num2 = 5;
+function funkcjaMoja(num1,num2)
+{
+if (num1>num2) {
+    console.log(num1);
+} else if (num1<num2){
+    console.log(num2);
+} else 
+    console.log ("Liczby są sobie równe");
+}
