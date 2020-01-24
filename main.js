27b2c6037a2be85308135187ee67814c35d47381
Paweł Migo <migo.pawel@gmail.com>
2020-01-24 18:29:21 +0100

    poprawka2

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var num1 = 2;
 var num2 = 5;
-function funkcjaMoja(num1,num2)
+function funkcjaMoja()
 {
-if (num1>num2){console.log(num1)} 
-else if (num1<num2){console.log(num2)} 
-else{console.log ("Liczby są sobie równe")}
-return (num1,num2);
+if (num1>num2){
+    return(num1)} 
+else if (num1<num2){
+    return(num2)} 
+else {
+    return("Liczby są sobie równe")}
+    console.log(num1,num2);
 }
+funkcjaMoja();
+
